e12d99223c2f1ef19ed2b1aeeff29eb5136cbb3d
huyhayho18 <giahuylevan1807@gmail.com>
2026-05-21 20:55:02 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -7,7 +7,8 @@ for month in range(1, month_count + 1):
     for branch in range(1, branch_count + 1):
         revenue[(branch, month)] = int(input(f"Nhập doanh thu Chi nhánh {branch}, tháng {month}: "))
 
-
 for month in range(1, month_count + 1):
     for branch in range(1, branch_count + 1):
         print(f"Chi nhánh {branch}, tháng {month}: {revenue[(branch, month)]} triệu đồng")
+
+# Bài 1 bị một lỗi sai locgic khi dùng vòng lặp lồng nhau doanh thu chi nhánh nằm trong tháng chính vì thế mà khi nhập bị lỗi sai không theo như mong muốn
