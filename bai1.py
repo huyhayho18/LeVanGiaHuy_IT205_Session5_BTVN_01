a6b5de876b122c479784c9d4ce0730de66a73c68
huyhayho18 <giahuylevan1807@gmail.com>
2026-05-21 20:48:02 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,13 @@
+branch_count = int(input("Nhập so lượng chi nhánh: "))
+month_count = 3
+
+revenue = {}
+
+for month in range(1, month_count + 1):
+    for branch in range(1, branch_count + 1):
+        revenue[(branch, month)] = int(input(f"Nhập doanh thu Chi nhánh {branch}, tháng {month}: "))
+
+
+for month in range(1, month_count + 1):
+    for branch in range(1, branch_count + 1):
+        print(f"Chi nhánh {branch}, tháng {month}: {revenue[(branch, month)]} triệu đồng")
